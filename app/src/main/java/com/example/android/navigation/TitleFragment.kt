4522b293dfde424c88e401bf3edd83ec3916c47e
madfree <matzefrei@gmail.com>
2020-08-12 16:31:02 +0200

    feat: Added conditional navigation according to trivia answers (correct/wrong)

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -20,8 +20,8 @@ class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val binding : FragmentTitleBinding = DataBindingUtil.inflate(inflater ,R.layout.fragment_title, container, false)
-        binding.playButton.setOnClickListener { Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment) }
+        val binding : FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+        binding.playButton.setOnClickListener (Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
         return binding.root;
     }
 
